a459c49c4543fe9a6617850488f97065eb79362d
鹈鹕 <tihu@myweimai.com>
2020-08-22 20:32:23 +0800

    feat: add forward

--- src/middlewares/forward.ts
+++ src/middlewares/forward.ts
@@ -1,5 +1,6 @@
 import { TMiddleware } from './type';
 
 export const forward: TMiddleware = () => next => async action => {
+  console.warn('Please check if your middleware forgot to handle actions.');
   await next(action);
 }
